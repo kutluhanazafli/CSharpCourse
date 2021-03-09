4ca1be87c80211189bdabcad1340432decdb1381
kutluhanazafli <kutluhan321@gmail.com>
2021-03-09 21:44:55 +0300

    Add project files.

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //Value Types
+
+           double number5 = 10.4;
+           decimal number6 = 10;
+           char character = 'A';
+           bool condition = false;
+           byte number4 = 255;
+           short number3 = 32767;
+           int number1 = 2147483647;   
+           long number2 = 9223372036854775807;
+
+           Console.WriteLine("Number1 is {0}", number1);
+           Console.WriteLine("Number2 is {0}", number2);
+           Console.WriteLine("Number3 is {0}", number3);
+           Console.WriteLine("Number4 is {0}", number4);
+           Console.WriteLine("Number5 is {0}", number5);
+           Console.WriteLine("Number6 is {0}", number6);
+           Console.WriteLine("Character is: {0}", (int) character);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
